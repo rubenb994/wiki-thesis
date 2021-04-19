d5e003489e59eeeb165c134d8af82b0c08c041c5
Ruben <34919304+rubenb994@users.noreply.github.com>
2021-04-19 22:22:45 +0200

    Initialized application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as SvgPanZoom from 'svg-pan-zoom';
+import { MatDialog } from '@angular/material/dialog';
+import { PopupComponent } from './components/popup/popup.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'wiki-thesis';
+export class AppComponent implements AfterViewInit {
+  svgPanZoomMap: SvgPanZoom.Instance;
+  readonly svgZoomFactor = 0.2;
+  readonly minZoom = 1;
+  readonly maxZoom = 3;
+
+  constructor(private dialog: MatDialog) {}
+
+  ngAfterViewInit(): void {
+    const svgPanZoomOptions: SvgPanZoom.Options = {
+      maxZoom: this.maxZoom,
+      minZoom: this.minZoom,
+      zoomScaleSensitivity: this.svgZoomFactor,
+      center: true,
+      beforePan: (oldPan, newPan) => {
+        // console.log(oldPan, newPan);
+      },
+    };
+    this.svgPanZoomMap = SvgPanZoom('#smylos-map', svgPanZoomOptions);
+  }
+
+  onClickZoomIn(): void {
+    const newZoom = this.svgPanZoomMap.getZoom() + this.svgZoomFactor;
+    if (newZoom > this.maxZoom) {
+      return;
+    }
+    this.svgPanZoomMap.zoom(newZoom);
+  }
+
+  onClickZoomOut(): void {
+    const newZoom = this.svgPanZoomMap.getZoom() - this.svgZoomFactor;
+    if (newZoom < this.minZoom) {
+      return;
+    }
+    this.svgPanZoomMap.zoom(newZoom);
+  }
+
+  onClickOpenDialog(): void {
+    this.dialog.open(PopupComponent);
+  }
+
+  onClickGroup(): void {
+    console.log('Group was clicked');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PopupComponent } from './components/popup/popup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PopupComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatDialogModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
