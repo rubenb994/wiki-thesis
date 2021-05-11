a1931ece7786aeb895071e1486d94d31b1d19838
Arthur <arthurvdwerf@hotmail.com>
2021-05-11 15:34:38 +0200

    Outro restart button

--- src/app/components/outro/outro.component.ts
+++ src/app/components/outro/outro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-outro',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+  }
+
+  onClickGoToMap(): void {
+    this.router.navigate(['map']);
   }
 
 }
